b6be827d0b01f078740878995c41af2cda195aeb
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-10-30 18:27:54 +0100

    issue with plugin backend
    Changed API URL
    
    axios.defaults.baseURL = 'http://localhost:3000/';

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+axios.defaults.baseURL = 'http://localhost:3000/';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
